4e2dbc071139a8b950e8731c2b7ae3b0e4a2b7c0
vox403 <java960419@gmail.com>
2026-06-23 00:02:38 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const SUPABASE_URL = "https://gvuhbxpkmyiakyfhuish.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_uCymLmzy-3NR9JyOL-9DtA_HmkGb0-k";
+export const PORTAL_STORAGE_KEY = "voxtek_employee_v1";
+
+export const MAX_NAME_LENGTH = 24;
+export const RANK_LIMIT = 40;
+export const ROLL_SPIN_INTERVAL = 150;
+export const ROLL_MIN_DURATION = 920;
